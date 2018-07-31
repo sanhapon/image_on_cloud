5f0d697e4014fb4ebea288634bfc625d85369c46
sanhapon <sanhapon@gmail.com>
2018-07-31 22:02:36 +0700

    add tel number field

--- src/containers/admin/RadioCenterPage.js
+++ src/containers/admin/RadioCenterPage.js
@@ -4,22 +4,19 @@ import RaisedButton from 'material-ui/RaisedButton';
 import MenuItem from 'material-ui/MenuItem';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
-import Toggle from 'material-ui/Toggle';
-import DatePicker from 'material-ui/DatePicker';
-import {grey400} from 'material-ui/styles/colors';
-import Divider from 'material-ui/Divider';
+import { grey400 } from 'material-ui/styles/colors';
 import PageBase from '../../components/PageBase';
 import province from '../../data/province';
 import getAmphor from '../../data/amphor';
 
 class RadioCenterPage extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = { 
-            theProvince : {pid:-1, name:'จังหวัด'} ,
-            theAmphor : {pid:-1, name:'อำเภอ'},
-            amphorList : []
+        this.state = {
+            theProvince: { pid: -1, name: 'จังหวัด' },
+            theAmphor: { pid: -1, name: 'อำเภอ' },
+            amphorList: []
         };
     }
 
@@ -44,63 +41,66 @@ class RadioCenterPage extends React.Component {
 
     handleProvinceChanged = (e, index, value) => {
         const amphorList = getAmphor(value.pid);
-        this.setState( {theProvince: {pid:value.pid, name:value.name}} ); 
-        this.setState( {amphorList:amphorList} );
+        this.setState({ theProvince: { pid: value.pid, name: value.name } });
+        this.setState({ amphorList: amphorList });
     }
 
     handleAmphorhanged = (e, index, value) => {
-        this.setState( {theAmphor: {pid:value.pid, name:value.name}} ); 
+        this.setState({ theAmphor: { pid: value.pid, name: value.name } });
     }
 
     render() {
-        const { theProvince, theAmphor, amphorList} = this.state;
+        const { theProvince, theAmphor, amphorList } = this.state;
 
         return (
-            <PageBase navigation='/admin/radioCenter' title='ใส่ข้อมูลศูนย์'>
+            <PageBase title='ใส่ข้อมูลศูนย์'>
                 <form>
                     <TextField
                         hintText="ชื่อศูนย์บริการ"
                         floatingLabelText="ชื่อศูนย์บริการ"
                         fullWidth={true}
                     />
+                    <TextField
+                        hintText="ที่อยู่ 1"
+                        floatingLabelText="ที่อยู่ 1"
+                        fullWidth={true}
+                    />
+                    <TextField
+                        hintText="ที่อยู่ 2"
+                        floatingLabelText="ที่อยู่ 2"
+                        fullWidth={true}
+                    />
                     <SelectField
                         floatingLabelText={theProvince.name}
                         fullWidth={true}
                         onChange={this.handleProvinceChanged}>
-                        { province.map((p) => <MenuItem key={p.pid} primaryText={p.name} value={p}/>) }
+                        {province.map((p) => <MenuItem key={p.pid} primaryText={p.name} value={p} />)}
                     </SelectField>
 
                     <SelectField
                         floatingLabelText={theAmphor.name}
                         fullWidth={true}
                         onChange={this.handleAmphorhanged}>
-                        { amphorList.map((p)=><MenuItem key={p.pid} primaryText={p.name} value={p}/>) }
-                       
+                        {amphorList.map((p) => <MenuItem key={p.pid} primaryText={p.name} value={p} />)}
+
                     </SelectField>
 
-                    <DatePicker
-                        hintText="Expiration Date"
-                        floatingLabelText="Expiration Date"
-                        fullWidth={true}/>
+                    <TextField
+                        hintText="หมายเลขโทรศัพท์"
+                        floatingLabelText="หมายเลขโทรศัพท์"
+                        fullWidth={true}
+                    />
+                    
+                    <div style={this.styles.buttons}>
+                        <Link to="/">
+                            <RaisedButton label="Cancel" />
+                        </Link>
 
-                    <div style={this.styles.toggleDiv}>
-                        <Toggle
-                            label="Disabled"
-                            labelStyle={this.styles.toggleLabel}
-                        />
+                        <RaisedButton label="Save"
+                            style={this.styles.saveButton}
+                            type="submit"
+                            primary={true} />
                     </div>
-
-                    <Divider/>
-                        <div style={this.styles.buttons}>
-                            <Link to="/">
-                                <RaisedButton label="Cancel"/>
-                            </Link>
-
-                            <RaisedButton label="Save"
-                                        style={this.styles.saveButton}
-                                        type="submit"
-                                        primary={true}/>
-                        </div>
                 </form>
             </PageBase>
         );
