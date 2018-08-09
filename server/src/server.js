5a7eb6aee8d3a67c0d3f01a1a20b24a80ed61f82
sanhapon <sanhapon@gmail.com>
2018-08-10 06:58:47 +0700

    work2

--- server/src/server.js
+++ server/src/server.js
@@ -2,33 +2,34 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 
-import webpack from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../../client/webpack.config.js';
+// import webpack from 'webpack';
+// import webpackMiddleware from 'webpack-dev-middleware';
+// import webpackHotMiddleware from 'webpack-hot-middleware';
+// import webpackConfig from '../../client/webpack.config.js';
 
 import { register } from './admin/register';
 
 const app = express();
 const centerRegiter = new register();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 
-const compiler = webpack(webpackConfig);
-app.use(webpackMiddleware(compiler, {
-    hot:true,
-    publicPath: webpackConfig.output.publicPath,
-    noInfo:true
-}));
-app.use(webpackHotMiddleware(compiler));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'dist')));
+//const compiler = webpack(webpackConfig);
+// app.use(webpackMiddleware(compiler, {
+//     hot:true,
+//     publicPath: webpackConfig.output.publicPath,
+//     noInfo:true
+// }));
+// app.use(webpackHotMiddleware(compiler));
 
 app.get('/api/center', (req,res) => {
     res.send(centerRegiter.getAllCenter());
 });
 
 app.post('/api/center', (req,res) => {
-    console.log('get sth');
+    
     const center = req.body;
     centerRegiter.registerCenter(center);
     const resp = {status:'done', msg:'เก็บข้อมูลเรียบร้อย'};
@@ -36,7 +37,8 @@ app.post('/api/center', (req,res) => {
     res.send(resp);
 });
 
-app.get('/*', (req, res)=> {
-    res.sendFile(path.join(__dirname, './index.html'));
+app.get('/*', (req, res)=> {  
+    res.sendFile(path.join(__dirname , 'dist', 'index.html'));
 });
 app.listen(3000);
+ 
