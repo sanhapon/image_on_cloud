799a2c20ebef71351353630560fcca8dc3b3d23f
sanhapon <sanhapon@gmail.com>
2018-08-26 21:59:45 +0700

    make it under construction

--- client/src/containers/admin/RadioPhotographerListPage.js
+++ client/src/containers/admin/RadioPhotographerListPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { FormControl } from '../../../node_modules/@material-ui/core';
+//import { FormControl } from '../../../node_modules/@material-ui/core';
 
 class RadioPhotoGrapherListPage extends React.Component {
     render() {
         return (
             <div>
-                <FormControl>
-                </FormControl>
+                Under Construction !!!
+                {/* <FormControl>
+                </FormControl> */}
             </div>
         );
     }
